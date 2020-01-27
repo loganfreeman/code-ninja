fc15403a6fa46b426d0a4fc36215809654b008d8
shanhong cheng <shanhongcheng@gmail.com>
2020-01-27 11:41:40 -0700

    first commit

---
+++ src/pages/tank.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+import styles from './tank.css';
+
+export default function() {
+  return (
+    <div className={styles.normal}>
+      <h1>Page tank</h1>
+    </div>
+  );
+}
